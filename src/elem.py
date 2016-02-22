19420fb7ded5e7ab29522ecea27db98f5a3f3103
Thomas DAVID <tdavid@gromalin.com>
2016-02-22 08:03:42 +0100

    Ajout d'une méthode de classe distance

--- src/elem.py
+++ src/elem.py
@@ -3,10 +3,13 @@
 
 import queue
 import unittest
-
+import math
 
 class Elem:
     id = 0
+
+    def distance (e1, e2):
+        return math.sqrt((e2.x-e1.x)**2 + (e2.y - e1.y)**2)
 
     def __init__(self, x, y):
         self.name = "{}_{}".format(self.__class__.__name__, self.__class__.id)
@@ -24,7 +27,7 @@ class Elem:
         self.queue.put(msg)
 
     def status(self):
-        return "{} ({},{})".format(self.name, self.x, self.y)
+        return "{} ({:.0f},{:.0f})".format(self.name, self.x, self.y)
 
     def get_msg(self):
         try:
@@ -35,18 +38,22 @@ class Elem:
 
 
 class TestElemMethods(unittest.TestCase):
-    elem = None
+    elem0 = None
+    elem1 = None
 
     def setUp(self):
-        self.elem = Elem(100, 100)
+        self.elem0 = Elem(100.01, 100.01)
+        self.elem1 = Elem(200.01, 200.01)
 
     def tearDown(self):
         Elem.id = 0
 
     def test_elem(self):
-        self.assertEquals("Elem_0", self.elem.__str__())
-        self.assertEquals("Elem_0 (100,100)", self.elem.status())
+        self.assertEquals("Elem_0", self.elem0.__str__())
+        self.assertEquals("Elem_0 (100,100)", self.elem0.status())
 
+    def test_distance(self):
+        self.assertAlmostEqual(141, Elem.distance(self.elem0, self.elem1),0)
 
 if __name__ == '__main__':
     unittest.main()
